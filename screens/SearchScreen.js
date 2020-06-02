a3dfe755a3892b7e0dafa1df43cf32c2089b1462
benjamintan <bentanjunrong@gmail.com>
2020-06-02 18:22:14 +0800

    Added List View

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,17 +1,46 @@
 import * as WebBrowser from 'expo-web-browser';
 import * as React from 'react';
 import { Image, Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
+import { ScrollView, FlatList } from 'react-native-gesture-handler';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 import { MonoText } from '../components/StyledText';
 import store from '../state/store';
+import { TextInput, Searchbar } from 'react-native-paper';
+
+function Item({ title }) {
+  return (
+    <View style = {styles.listItem}>
+      <Text style= {styles.title}>{title}</Text>
+    </View>
+  )
+}
 
 export default function SearchScreen() {
+  const data = [
+    {
+      compound_code: "1234"
+    },
+    {
+      compound_code: "1234"
+    },
+    {
+      compound_code: "1234"
+    },
+  ];
   return (
     <View style={styles.container}>
-        <Text>
-            SEARCHSCREEN HERE
-        </Text>
+        <Searchbar style={styles.search} placeholder = "Search for a location"
+        />
+        <SafeAreaView style = {{flex:12}}>
+          <FlatList
+            data={data}
+            renderItem={({ item }) =>
+              <Item title = {item.compound_code}/>
+            }
+            keyExtractor={item => item.id}
+          />
+        </SafeAreaView>
     </View>
   );
 }
@@ -20,5 +49,22 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-  }
+    padding: 100
+  }, 
+  search : {
+    padding : 0,
+    flex: 1,
+  },
+  name: {
+    fontWeight: "bold",
+    fontSize: 36
+  },
+  listItem: {
+    marginHorizontal: 32,
+    marginVertical: 10,
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between'
+  },
 });
