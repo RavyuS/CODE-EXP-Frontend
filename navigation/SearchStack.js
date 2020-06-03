ff1b27f424fc55b10414cc772f995ffcb4949f03
benjamintan <bentanjunrong@gmail.com>
2020-06-04 05:19:45 +0800

    Changed header colour

--- navigation/SearchStack.js
+++ navigation/SearchStack.js
@@ -18,7 +18,6 @@ export default function SearchStackNavigator({ navigation, route }) {
   navigation.setOptions({ headerTitle: getHeaderTitle(route) });
 
   return (
-<<<<<<< HEAD
     <SearchStack.Navigator initialRouteName={INITIAL_ROUTE_NAME} 
       screenOptions={{
         headerStyle: {
@@ -29,9 +28,6 @@ export default function SearchStackNavigator({ navigation, route }) {
           fontWeight: 'bold',
         },
       }}>
-=======
-    <SearchStack.Navigator initialRouteName={INITIAL_ROUTE_NAME} screenOptions={{flex:1}}>
->>>>>>> 8199f03c182ce8b84586dbdf07070f32bfd2f632
       <SearchStack.Screen
         name="SearchInfo"
         component={SearchScreen}
