bcc225593742283408421950f0ca48cf636c4f2c
RavyuS <ravyusk@gmail.com>
2020-06-04 00:40:37 +0800

    Added more items to default search list

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -14,67 +14,70 @@ import store from '../state/store';
 import { TextInput, Searchbar, Button } from 'react-native-paper';
 
 function Item({ address, title, onSelect }) {
-  
+
 }
+
+
 export default function SearchScreen({ navigation }) {
-  let placeDetails = { 
-    compoundCode : "9V2C+CQ Singapore",
-    formattedAddress : "23 Serangoon Central, #03-42 Nex Mall, Singapore 556083",
-    name : "FairPrice Xtra Nex Mall"
+  let placeDetails = {
+    compoundCode: "9V2C+CQ Singapore",
+    formattedAddress: "23 Serangoon Central, #03-42 Nex Mall, Singapore 556083",
+    name: "FairPrice Xtra Nex Mall"
   }
   // once we set up history in store, no need to declare placedetails early
-  const [history, setHistory] = useState([placeDetails]);
+  const [history, setHistory] = useState(preparedPlaces);
   const [searchText, setSearchText] = useState("")
 
   _onSearch = () => axios.get(`${apiURL}/api/googleapi?name=${searchText}`)
-    .then(res =>{
+    .then(res => {
       console.log(res);
-      placeDetails = { 
-        compoundCode : res.data.candidates[0].plus_code.compound_code,
-        formattedAddress : res.data.candidates[0].formatted_address,
-        name : res.data.candidates[0].name
+      placeDetails = {
+        compoundCode: res.data.candidates[0].plus_code.compound_code,
+        formattedAddress: res.data.candidates[0].formatted_address,
+        name: res.data.candidates[0].name
       };
       if (!history.includes([placeDetails])) {
         setHistory(history => [placeDetails].concat(history));
       }
-      
+
       navigation.navigate('PlaceInfo', placeDetails);
-    }).catch(error =>{console.log(error)}
-  );
+    }).catch(error => { console.log(error) }
+    );
 
   onSelect = () => {
-    navigation.navigate('PlaceInfo',placeDetails)
+    navigation.navigate('PlaceInfo', placeDetails)
   }
 
   return (
     <View style={styles.container}>
-        <Searchbar 
-          style={styles.search} 
-          placeholder = "Search for a location"
-          icon={() => <MaterialCommunityIcons name="map-search" size={30}/>}
-          onChangeText = {(value) => setSearchText(value)}
-          onIconPress={this._onSearch}
+      <Searchbar
+        style={styles.search}
+        placeholder="Search for a location"
+        icon={() => <MaterialCommunityIcons name="map-search" size={30} />}
+        onChangeText={(value) => setSearchText(value)}
+        onIconPress={this._onSearch}
+      />
+      <SafeAreaView style={{ flex: 12 }}>
+        <FlatList
+          data={history}
+          renderItem={({ item }) => {
+            return (
+              <TouchableOpacity
+                onPress={this.onSelect}
+                style={[
+                  styles.listItem,
+                  { backgroundColor: '#f9c2ff' },
+                ]}
+              >
+                <Text style={styles.title}>{item.name}</Text>
+                <Text style={styles.address}>{item.formattedAddress}</Text>
+              </TouchableOpacity>
+            );
+          }
+          }
+          keyExtractor={item => item.id}
         />
-        <SafeAreaView style = {{flex:12}}>
-          <FlatList
-            data={history}
-            renderItem={({ item }) => {
-              return (
-                <TouchableOpacity
-                  onPress={this.onSelect}
-                  style={[
-                    styles.listItem,
-                    { backgroundColor: '#f9c2ff'},
-                  ]}
-                >
-                  <Text style={styles.title}>{item.name}</Text>
-                  <Text style={styles.address}>{item.formattedAddress}</Text>
-                </TouchableOpacity>
-              );}
-            }
-            keyExtractor={item => item.id}
-          />
-        </SafeAreaView>
+      </SafeAreaView>
     </View>
   );
 }
@@ -84,9 +87,9 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     padding: 10
-  }, 
-  search : {
-    padding : 0,
+  },
+  search: {
+    padding: 0,
     flex: 1,
   },
   name: {
@@ -102,3 +105,43 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between'
   },
 });
+
+
+const preparedPlaces = [
+  {
+    compoundCode: "9V2C+CQ Singapore",
+    formattedAddress: "23 Serangoon Central, #03-42 Nex Mall, Singapore 556083",
+    name: "FairPrice Xtra Nex Mall"
+  },
+  {
+    compoundCode: "7RRW+7C Singapore",
+    formattedAddress: "Clark Quay, Singapore",
+    name: "Zouk Singapore"
+  },
+  {
+    compoundCode: "CQ3V+P6 Singapore",
+    formattedAddress: "Mandai, Singapore",
+    name: "Singapore Zoo"
+  },
+  {
+    compoundCode: "9V5P+Q3 Singapore",
+    formattedAddress: "Heartland Mall, Singapore",
+    name: "Safarees The Barbershop Heartland Mall"
+  },
+  {
+    compoundCode: "9V2C+9R Singapore",
+    formattedAddress: "Nex, Singapore",
+    name: "Koi Nex"
+  },
+  {
+    compoundCode: "7VJ7+JC Singapore",
+    formattedAddress: "Singapore",
+    name: "Gardens by the Bay"   
+  },
+  {
+    compoundCode: "7VP5+4P Singapore",
+    formattedAddress: "Singapore",
+    name: "Marina Bay Sands Garden Park"  
+  }
+
+]
